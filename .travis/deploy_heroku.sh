83169554cdb778a2a8eda1fd64e0098c2b607358
Vladimir K <vladimir.sdev@gmail.com>
2018-05-10 17:45:01 +0300

    Update heroku

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# heroku plugins:install heroku-container-registry
+# heroku container:login
+# docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+# heroku container:push web --app $HEROKU_APP_NAME
+
+docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+docker build -t registry.heroku.com/$HEROKU_APP_NAME/web .
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
